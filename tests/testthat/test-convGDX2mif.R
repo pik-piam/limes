8de1c2b922324be5fed34a3d93ecb3b58c2574bd
Sebastian Osorio <osorio@LT995.pik-potsdam.de>
2022-08-03 17:42:24 +0200

    New test on an old and new gdx

--- tests/testthat/test-convGDX2mif.R
+++ tests/testthat/test-convGDX2mif.R
@@ -1,39 +1,44 @@
 context("LIMES reporting")
 
 test_that("Test if limes reporting is produced as it should", {
-  
+
   library(gdx)
   unzip(system.file("extdata","fulldata.zip",package = "limes"))
-  gdxs <- "fulldata.gdx"
+  gdxs <- c("fulldata_20220119.gdx",
+            "fulldata_20220803.gdx")
 
   runTests <- function(gdxs=NULL){
-    
+
     skip_if_not(as.logical(gdxrrw::igdx(silent = TRUE)), "gdxrrw is not initialized properly")
-    
+
     a <- convGDX2MIF(gdxs)
   }
 
-  expect_error(runTests(gdxs),regexp = NA)
-  unlink(gdxs)
-  
+  for(gdx in gdxs) {
+    expect_error(runTests(gdx),regexp = NA)
+    unlink(gdx)
+  }
 
 })
 
 test_that("Test if limes slice reporting is produced as it should", {
-  
+
   library(gdx)
   unzip(system.file("extdata","fulldata.zip",package = "limes"))
-  gdxs <- "fulldata.gdx"
-  
+  gdxs <- c("fulldata_20220119.gdx",
+            "fulldata_20220803.gdx")
+
   runTests <- function(gdxs=NULL){
-    
+
     skip_if_not(as.logical(gdxrrw::igdx(silent = TRUE)), "gdxrrw is not initialized properly")
-    
+
     a <- convGDX2MIF_tau(gdxs)
   }
-  
-  expect_error(runTests(gdxs),regexp = NA)
-  unlink(gdxs)
-  
-  
+
+  for(gdx in gdxs) {
+    expect_error(runTests(gdx),regexp = NA)
+    unlink(gdx)
+  }
+
+
 })
