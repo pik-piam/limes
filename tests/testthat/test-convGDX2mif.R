210e68272c433095a8646fcd0284f7de0ad7c161
Antoine Levesque <levesque@pik-potsdam.de>
2022-08-04 14:48:49 +0200

    outsourced fulldatagdx for testing

--- tests/testthat/test-convGDX2mif.R
+++ tests/testthat/test-convGDX2mif.R
@@ -1,39 +1,41 @@
 context("LIMES reporting")
 
-test_that("Test if limes reporting is produced as it should", {
-  
-  library(gdx)
-  unzip(system.file("extdata","fulldata.zip",package = "limes"))
-  gdxs <- "fulldata.gdx"
+# add GDXs for comparison here:
+gdxPaths <- NULL
 
-  runTests <- function(gdxs=NULL){
-    
-    skip_if_not(as.logical(gdxrrw::igdx(silent = TRUE)), "gdxrrw is not initialized properly")
-    
-    a <- convGDX2MIF(gdxs)
+if (length(gdxPaths) == 0) {
+  defaultGdxPath <- file.path(tempdir(), "fulldata.gdx")
+  if (!file.exists(defaultGdxPath)) {
+    utils::download.file("https://rse.pik-potsdam.de/data/example/fulldata_LIMES.gdx",
+                         defaultGdxPath, mode = "wb", quiet = TRUE)
   }
+  gdxPaths <- defaultGdxPath
+}
 
-  expect_error(runTests(gdxs),regexp = NA)
-  unlink(gdxs)
-  
+runTests <- function(gdxs = NULL, timeSlice = FALSE) {
+
+  skip_if_not(as.logical(gdxrrw::igdx(silent = TRUE)), "gdxrrw is not initialized properly")
+  if (! timeSlice) {
+    a <- convGDX2MIF(gdxs)
+  } else {
+    a <- convGDX2MIF_tau(gdxs)
+  }
+  return(a)
+}
+
+test_that("Test if limes reporting is produced as it should", {
+
+  expect_error(runTests(gdxPaths), regexp = NA)
 
 })
 
 test_that("Test if limes slice reporting is produced as it should", {
-  
-  library(gdx)
-  unzip(system.file("extdata","fulldata.zip",package = "limes"))
-  gdxs <- "fulldata.gdx"
-  
-  runTests <- function(gdxs=NULL){
-    
-    skip_if_not(as.logical(gdxrrw::igdx(silent = TRUE)), "gdxrrw is not initialized properly")
-    
-    a <- convGDX2MIF_tau(gdxs)
-  }
-  
-  expect_error(runTests(gdxs),regexp = NA)
-  unlink(gdxs)
-  
-  
+
+  expect_error(runTests(gdxPaths, timeSlice = TRUE), regexp = NA)
+
+
 })
+
+
+unlink(tempdir(), recursive = TRUE)
+tempdir(TRUE)
