a756cdc0be43049f6ed405cc55a19f3357d4d640
sosorior <osorio@pik-potsdam.de>
2022-08-04 12:49:02 +0200

    Revert "New test on an old and new gdx"
    This reverts commit 8de1c2b922324be5fed34a3d93ecb3b58c2574bd.

--- tests/testthat/test-convGDX2mif.R
+++ tests/testthat/test-convGDX2mif.R
@@ -1,44 +1,39 @@
 context("LIMES reporting")
 
 test_that("Test if limes reporting is produced as it should", {
-
+  
   library(gdx)
   unzip(system.file("extdata","fulldata.zip",package = "limes"))
-  gdxs <- c("fulldata_20220119.gdx",
-            "fulldata_20220803.gdx")
+  gdxs <- "fulldata.gdx"
 
   runTests <- function(gdxs=NULL){
-
+    
     skip_if_not(as.logical(gdxrrw::igdx(silent = TRUE)), "gdxrrw is not initialized properly")
-
+    
     a <- convGDX2MIF(gdxs)
   }
 
-  for(gdx in gdxs) {
-    expect_error(runTests(gdx),regexp = NA)
-    unlink(gdx)
-  }
+  expect_error(runTests(gdxs),regexp = NA)
+  unlink(gdxs)
+  
 
 })
 
 test_that("Test if limes slice reporting is produced as it should", {
-
+  
   library(gdx)
   unzip(system.file("extdata","fulldata.zip",package = "limes"))
-  gdxs <- c("fulldata_20220119.gdx",
-            "fulldata_20220803.gdx")
-
+  gdxs <- "fulldata.gdx"
+  
   runTests <- function(gdxs=NULL){
-
+    
     skip_if_not(as.logical(gdxrrw::igdx(silent = TRUE)), "gdxrrw is not initialized properly")
-
+    
     a <- convGDX2MIF_tau(gdxs)
   }
-
-  for(gdx in gdxs) {
-    expect_error(runTests(gdx),regexp = NA)
-    unlink(gdx)
-  }
-
-
+  
+  expect_error(runTests(gdxs),regexp = NA)
+  unlink(gdxs)
+  
+  
 })
