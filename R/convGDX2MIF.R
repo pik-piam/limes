49577188290775dd6025b462529c6793c499aa69
sosorior <osorio@pik-potsdam.de>
2024-09-23 12:37:15 +0200

    add new industry variables and module

--- R/convGDX2MIF.R
+++ R/convGDX2MIF.R
@@ -62,7 +62,7 @@ convGDX2MIF <- function(gdx,gdx_ref=NULL,file=NULL,scenario="default", time=as.n
   output <- mbind(output,reportIndustryEmissions(gdx,output)[,time,]) #depending on CO2 price and emissions
 
   #adding the new industry module
-  #output <- mbind(output,reportIndustryModule(gdx)[,time,]) #depending on CO2 price and emissions
+  output <- mbind(output,reportIndustryModule(gdx)[,time,])
 
   #adding capital costs to report output
   output <- mbind(output,reportCapitalCosts(gdx)[,time,])
